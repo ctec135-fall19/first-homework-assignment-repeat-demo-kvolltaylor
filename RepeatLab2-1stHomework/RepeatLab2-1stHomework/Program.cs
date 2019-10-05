1a2d053624729fef7c01b1e98c3eb7537343fe47
Katrina <k.voll-taylor@students.clark.edu>
2019-10-05 11:42:00 -0700

    Repeat Lab 2 1st Homework Assignment submission

---
+++ RepeatLab2-1stHomework/RepeatLab2-1stHomework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatLab2_1stHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            SportsCar myCar = new SportsCar();
+            myCar.PetName = "Ethel";
+            Console.WriteLine("My sports car : {0}", myCar.getPetName(), myCar.PetName);
+
+
+        }
+    }
+}
---
+++ RepeatLab2-1stHomework/RepeatLab2-1stHomework/SportsCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatLab2_1stHomework
+{
+    class SportsCar : Car
+    {
+        public string getPetName()
+        {
+            return PetName;
+        }
+    }
+}
